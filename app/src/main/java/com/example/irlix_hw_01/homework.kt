9995385a34c23a217571c91ac76f110aa965dee9
Nastya Bogodukhova <bonastya@gmail.com>
2022-02-16 23:19:25 +0400

    interface Publication, Book & Magazine class

--- app/src/main/java/com/example/irlix_hw_01/homework.kt
+++ app/src/main/java/com/example/irlix_hw_01/homework.kt
@@ -1,2 +1,28 @@
 package com.example.irlix_hw_01
 
+interface Publication {
+    val price:Double
+    val wordCount:Int
+
+    fun getType():String {
+        return ""
+    }
+}
+
+class Book(override val price: Double, override val wordCount: Int) : Publication {
+
+    override fun getType():String {
+        if(wordCount<=1000)
+            return "Flash Fiction"
+        else if(wordCount<=7500)
+            return "Short Story"
+        else
+            return "Novel"
+    }
+}
+
+class Magazine(override val price: Double, override val wordCount: Int) : Publication {
+    override fun getType():String {
+        return "Magazine"
+    }
+}
